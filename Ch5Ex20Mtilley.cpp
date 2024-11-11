a0f478061689eaf55385b09cbec690648bd17e45
Mat26045 <mat26045@email.vccs.edu>
2024-11-11 12:31:47 -0500

    Update Ch5Ex20Mtilley.cpp

--- Ch5Ex20Mtilley.cpp
+++ Ch5Ex20Mtilley.cpp
@@ -5,27 +5,31 @@
 using namespace std;
 
 void guessingGame() {
-       srand(time(0));  
+        srand(time(0));  
     int random_number = rand() % 100 + 1;
 
     int guess;
+    int guess_count = 0; 
 
     cout << "Welcome to the Random Number Guessing Game!" << endl;
     cout << "I'm thinking of a number between 1 and 100." << endl;
 
     while (true) {
-            cout << "Enter your guess: ";
+             cout << "Enter your guess: ";
         cin >> guess;
 
-         if (guess < random_number) {
+          guess_count++;
+
+               if (guess < random_number) {
             cout << "Too low, try again." << endl;
         }
         else if (guess > random_number) {
             cout << "Too high, try again." << endl;
         }
         else {
-            cout << "Congratulations! You guessed the correct number." << endl;
-            break; 
+            cout << "Congratulations! You guessed the correct number!" << endl;
+            cout << "It took you " << guess_count << " guesses." << endl;
+            break;  
         }
     }
 }
